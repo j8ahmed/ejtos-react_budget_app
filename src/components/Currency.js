a359d6d7bc0f9bfe90334031e610376853a0de4e
Jamal Ahmed <j8ahmed@gmail.com>
2024-02-29 10:40:29 -0500

    Add a currency converter

---
+++ src/components/Currency.js
@@ -0,0 +1,41 @@
+import React, { useContext, useState } from 'react';
+import { AppContext } from '../context/AppContext';
+
+const currencyOptions = {
+    "$": "$ Dollar",
+    "£": "£ Pound",
+    "€": "€ Euro",
+    "₹": "₹ Ruppee",
+}
+
+export default function Currency() {
+    const {dispatch, currency} = useContext(AppContext);
+    const [newCurrency, setNewCurrency] = useState(currency);
+
+    const handleChange = (event) => {
+        //console.log(currency)
+        const val = event.target.value
+        if(val !== currency){
+            setNewCurrency(val);
+            dispatch({
+                type: "CHG_CURRENCY",
+                payload: val
+            })
+        }
+    }
+    return (
+        <div className='alert alert-secondary'>
+            <label for="currency-selector">Select a Currency:</label>
+            <select id="currency-selector" name="currency" value={currency} onChange={handleChange}
+                style={{width: "100%"}}
+            >
+                <option value={currency} selected>Currency ({currencyOptions[currency]})</option>
+                {
+                    Object.keys(currencyOptions).map((key, i) => (
+                        <option key={i} value={key}>{currencyOptions[key]}</option>
+                    ))
+                }
+            </select>
+        </div>
+    );
+};
