348abfdfdfd2bc6dc98ffd2d288fd0298c7888ee
Jamal Ahmed <j8ahmed@gmail.com>
2024-02-29 11:28:23 -0500

    connect budget to app state

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,14 +2,31 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { dispatch, budget, expenses, currency } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
+    const upperLimit = 20000;
+    const totalExpenses = expenses.reduce((a,b) => a+b.cost, 0);
+
     const handleBudgetChange = (event) => {
-        setNewBudget(event.target.value);
+        const val =  event.target.value
+        if(val > upperLimit) {
+            alert(`${val} £ is too high. The budget value cannot exceed ${upperLimit} £`);
+            return;
+        } else if (val < totalExpenses) {
+            const minval = val === "" && 0;
+            alert(`${minval} £ is too low. The budget value cannot be less than the total expenses (${totalExpenses}) £`);
+            return;
+        }else {
+            setNewBudget(val);
+            dispatch({
+                type: "SET_BUDGET",
+                payload: val
+            })
+        }
     }
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <span>Budget: {currency} </span>
             <input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
         </div>
     );
